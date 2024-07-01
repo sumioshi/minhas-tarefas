ba9ab463b2eb7f0329d10ddc5d51e4fba8821ebc
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-01 10:40:19 -0300

    parametrizando os filtros

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+  box-sizing: border-box;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+  width: 100%;
+  box-sizing: border-box;
+`
+
+export const Campo = styled.input`
+  padding: 8px;
+  background-color: #ffff;
+  border-radius: 8px;
+  font-weight: bold;
+  border: 1px solid #666666;
+  width: 100%;
+  box-sizing: border-box;
+`
