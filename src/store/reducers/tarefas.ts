9193bb1da42a77d9350d6efcf45155a5d6c20dc3
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-03 11:33:02 -0300

    configurando redux

---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o exercicio do módulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Praticar a construção de uma Landing Page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar BootStrap'
+    }
+  ]
+}
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    }
+  }
+})
+
+export const { remover } = tarefasSlice.actions
+
+export default tarefasSlice.reducer
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,8 +4,9 @@ const EstiloGlobal = createGlobalStyle`
   * {
   margin: 0;
   padding: 0;
-  box- sizing: border-box;
+  box-sizing: border-box;
   font-family: Roboto, sans-serif;
+  list-style: none;
   }
   `
 
