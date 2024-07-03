e81d118f0c6c7227050c3eb3fd363323eac98804
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-03 17:59:23 -0300

    Navegue entre rotas

---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Circulo = styled(Link)`
+  height: 75px;
+  width: 75px;
+  background-color: #44bd32;
+  color: #fff;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  text-decoration: none;
+`
